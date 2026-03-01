8f5814f7b0d6837bdbfa650cdcb3fa820784e0da
manalr511 <manalr596@gmail.com>
2026-03-01 13:37:01 +0500

    edited

--- w3/HT13.cpp
+++ w3/HT13.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main (){
 
-int int1;
-cout<<" enter intergar1 :";
-cin>>int1;
-int int2;
-cout<<" enter intergar2 :";
-cin>>int2;
-int int3;
-cout<<" enter intergar3 :";
-cin>>int3;
-int int4;
-cout<<" enter intergar4 :";
-cin>>int4;
-int int5;
-cout<<" enter intergar5 :";
-cin>>int5;
-int sum;
-sum = int1+int2+int3+int4+int5;
-cout<<" sum = " <<sum;
+int num,sum=0;
+cout<<" enter 5 integers : \n ";
+cin>>num;
+sum= sum + num;
+cin>>num;
+sum= sum + num;
+cin>>num;
+sum= sum + num;
+cin>>num;
+sum= sum + num;
+cin>>num;
+sum= sum + num;
+cout<<" the sum is : "<<sum;
+
 
 }
